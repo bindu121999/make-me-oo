7921eb49777284ed74026d4b8292165a9b9b1caa
Bindu A <binduakalale@gmail.com>
2021-04-19 13:35:27 +0530

    Refactor. Add Functions in Point class to calculate x and y distance

--- src/main/java/org/oop/Point.java
+++ src/main/java/org/oop/Point.java
@@ -10,14 +10,22 @@ public class Point {
     }
 
     public double distance(Point to){
-        double xDistance = to.x - this.x;
-        double yDistance = to.y - this.y;
+        double xDistance = getxDistance(to);
+        double yDistance = getyDistance(to);
         return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
     }
 
     public double direction(Point to){
-        double xDistance = to.x - this.x;
-        double yDistance = to.y - this.y;
+        double xDistance = getxDistance(to);
+        double yDistance = getyDistance(to);
         return Math.atan2(yDistance, xDistance);
+    }
+
+    private double getxDistance(Point to){
+        return to.x-this.x;
+    }
+
+    private double getyDistance(Point to){
+        return to.y-this.y;
     }
 }
