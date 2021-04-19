deea01d0e994e5ee0aeec232e73087810562e60f
Bindu A <binduakalale@gmail.com>
2021-04-19 13:15:50 +0530

    Add. Two Functions Distance and Direction in Point Class

--- src/main/java/org/oop/Point.java
+++ src/main/java/org/oop/Point.java
@@ -24,4 +24,16 @@ public class Point {
         this.x = x;
         this.y = y;
     }
+
+    public double distance(Point to){
+        double xDistance = to.x - this.x;
+        double yDistance = to.y - this.y;
+        return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+    }
+
+    public double direction(Point to){
+        double xDistance = to.x - this.x;
+        double yDistance = to.y - this.y;
+        return Math.atan2(yDistance, xDistance);
+    }
 }
